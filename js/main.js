adf8d657c208312b16cfa56063e4accc36abce94
JSNinad <ninadshetty2003@gmail.com>
2023-10-01 21:09:58 +0530

    url1

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ setTimeout(() => {
 
 //Email Capture
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw6yGTk9SWrCS3ovq3bJEcVZ78c4SQ88q5zJEwzHSvoS1lrWjaCLq3xfIDma9c6qEhF/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzQ0njL7WdOaWivOPKgHZfz2XfVSjf3nIM0--T5_96UMy6UqrasEPsRhnwe_DZMHy6kfA/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg");
 
